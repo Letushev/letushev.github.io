6f00f8707d444ad2c517953343b601f2278a056b
Letushev <o.m.letushev@gmail.com>
2021-02-05 00:03:55 +0200

    shuffle array + qa image support

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+
+  return array;
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const subject = urlParams.get('s');
 
@@ -8,18 +17,49 @@ if (subject === 'algebra' || subject === 'geometry') {
   const nextButton = document.querySelector('.nav__button--next');
   const prevButton = document.querySelector('.nav__button--prev');
 
-  const qa = data[subject];
+  const qa = shuffleArray([...data[subject]]);
   let current = 0;
   let isAnswerVisible = false;
 
+  document.querySelector('.nav__subject').textContent = 
+    subject === 'algebra' ? 'Алгебра' : 'Геометрія';
+
+  const formatText = string => {
+    if (string.includes('images/')) {
+      const img = document.createElement('img');
+      img.setAttribute('alt', '');
+      img.setAttribute('src', string);
+      img.className = "qa__img";
+      return img;
+    }
+
+    const lines = string.split(/\r?\n/);
+
+    if (lines.length > 1) {
+      const list = document.createElement('ul');
+      for (let line of lines) {
+        const item = document.createElement('li');
+        item.textContent = line;
+        list.appendChild(item);
+      }
+      return list;
+    }
+
+    const p = document.createElement('p');
+    p.textContent = lines[0];
+    return p;
+  }
+
   const showAnswer = () => {
-    answerEl.innerHTML = qa[current].a;
+    answerEl.innerHTML = '';
+    answerEl.appendChild(formatText(qa[current].a));
     isAnswerVisible = true;
   }
 
   const updateAnswer = () => {
     scroreEl.textContent = `${current + 1} / ${qa.length}`;
-    questionEl.innerHTML = qa[current].q;
+    questionEl.innerHTML = '';
+    questionEl.appendChild(formatText(qa[current].q))
 
     prevButton.style.visibility = current === 0 ? 'hidden' : 'visible';
     nextButton.style.visibility = current === qa.length - 1 ? 'hidden' : 'visible';
@@ -28,11 +68,12 @@ if (subject === 'algebra' || subject === 'geometry') {
       const img = document.createElement('img');
       img.setAttribute('src', 'images/question.svg');
       img.setAttribute('alt', '');
+      img.className = 'qa__mark';
       img.addEventListener('click', () => {
         showAnswer();
       });
 
-      answerEl.textContent = "";
+      answerEl.innerHTML = '';
       answerEl.appendChild(img);
       isAnswerVisible = false;
     }
