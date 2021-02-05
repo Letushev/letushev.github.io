035885a669408b615d07f3418d646f5e101a96cc
Letushev <o.m.letushev@gmail.com>
2021-02-05 12:29:51 +0200

    add some data

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 const data = {
   algebra: [
+  ],
+  geometry: [
     {
       q: "Які трикутники називаються подібними?",
       a: "Трикутники називаються подібними, якщо у них рівні кути та пропорційні відповідні сторони."
@@ -13,21 +15,47 @@ const data = {
       `
     },
     {
-      q: "images/algebra/1-q.jpg",
-      a: "1"
+      q: "images/geometry/1-q.jpg",
+      a: "images/geometry/1-a.jpg"
     },
     {
-      q: "2",
-      a: "images/algebra/2-a.jpg"
+      q: "images/geometry/2-q.jpg",
+      a: "images/geometry/2-a.jpg"
     },
     {
-      q: "3",
-      a: "3"
+      q: "images/geometry/3-q.jpg",
+      a: "images/geometry/3-a.jpg"
     },
     {
-      q: "4",
-      a: "4"
+      q: "images/geometry/4-q.jpg",
+      a: "images/geometry/4-a.jpg"
+    },
+    {
+      q: "images/geometry/5-q.jpg",
+      a: "images/geometry/5-a.jpg"
+    },
+    {
+      q: "images/geometry/6-q.jpg",
+      a: "images/geometry/6-a.jpg"
+    },
+    {
+      q: "images/geometry/7-q.jpg",
+      a: "images/geometry/7-a.jpg"
+    },
+    {
+      q: "images/geometry/8-q.jpg",
+      a: "images/geometry/8-a.jpg"
+    },{
+      q: "images/geometry/9-q.jpg",
+      a: "images/geometry/9-a.jpg"
+    },
+    {
+      q: "images/geometry/10-q.jpg",
+      a: "images/geometry/10-a.jpg"
+    },
+    {
+      q: "images/geometry/11-q.jpg",
+      a: "images/geometry/11-a.jpg"
     }
   ],
-  geometry: [],
 };
